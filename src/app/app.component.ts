c0892133fbe01f4dba2b516e679b8e43309096e6
Sangram-mohanty <sangrammohanty21@gmail.com>
2024-06-30 22:24:09 +0530

    drakviewcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DarkModeService } from './services/dark-mode.service';
+import { ContentComponent } from "./content/content.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, CommonModule, ContentComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'darkModeUsingTailwind';
+  darkModeService:DarkModeService =inject(DarkModeService)
 }
